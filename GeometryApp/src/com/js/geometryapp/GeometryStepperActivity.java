9f162c87bdad2ffaaf1afb5d7adcb7f72cf7753f
Jeff Sember <jpsember@gmail.com>
2014-09-21 19:50:39 -0700

    Added SlidingPaneLayout to hold user options

--- GeometryApp/src/com/js/geometryapp/GeometryStepperActivity.java
+++ GeometryApp/src/com/js/geometryapp/GeometryStepperActivity.java
@@ -1,15 +1,30 @@
 package com.js.geometryapp;
 
+import com.js.android.MyActivity;
+
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import static com.js.basic.Tools.*;
 
 public abstract class GeometryStepperActivity extends GeometryActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		doNothing();
 		mAlgorithmStepper = AlgorithmStepper.sharedInstance();
+		hideTitle();
+
 		super.onCreate(savedInstanceState);
 		mAlgorithmStepper.restoreInstanceState(savedInstanceState);
 	}
@@ -26,14 +41,150 @@ public abstract class GeometryStepperActivity extends GeometryActivity {
 		super.onDestroy();
 	}
 
+	/**
+	 * Hide the title bar, to conserve screen real estate
+	 */
+	private void hideTitle() {
+		boolean hideTitle = true;
+		// Checking this causes a null pointer exception if device is
+		// rotated
+		if (false) {
+			DisplayMetrics m = MyActivity.displayMetrics();
+			boolean landscapeMode = m.widthPixels > m.heightPixels;
+			if (!landscapeMode)
+				hideTitle = false;
+		}
+		if (hideTitle)
+			getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+	}
+
 	public void setAlgorithmDelegate(AlgorithmStepper.Delegate delegate) {
 		mAlgorithmStepper.setDelegate(delegate);
 	}
 
+	/**
+	 * Construct a DrawerLayout that contains the main view and an auxilliary
+	 * view
+	 * 
+	 * @param mainView
+	 * @param auxilliaryView
+	 * @deprecated
+	 */
+	private ViewGroup constructDrawerLayout(View mainView, View auxilliaryView) {
+		DrawerLayout drawer = new DrawerLayout(this);
+
+		DrawerLayout.LayoutParams lp = new DrawerLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.MATCH_PARENT);
+
+		drawer.addView(mainView, lp);
+
+		lp = new DrawerLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.MATCH_PARENT);
+		lp.gravity = Gravity.START;
+		drawer.addView(auxilliaryView, lp);
+
+		return drawer;
+	}
+
+	/**
+	 * Construct a SlidingPaneLayout that contains the main view and an
+	 * auxilliary view
+	 * 
+	 * @param mainView
+	 * @param auxilliaryView
+	 */
+	private ViewGroup constructSlidingPaneLayout(View mainView,
+			View auxilliaryView) {
+
+		SlidingPaneLayout container = new SlidingPaneLayout(this);
+
+		// Determine if device in current orientation is large enough to display
+		// both panes at once
+
+		DisplayMetrics displayMetrics = MyActivity.displayMetrics();
+		float mainWidth;
+		float optionsWidth;
+
+		final float PREFERRED_MAIN_WIDTH_INCHES = 3;
+		final float PREFERRED_OPTIONS_WIDTH_INCHES = 2;
+		final float TOTAL_WIDTH_INCHES = (PREFERRED_MAIN_WIDTH_INCHES + PREFERRED_OPTIONS_WIDTH_INCHES);
+
+		boolean landscapeMode = displayMetrics.widthPixels > displayMetrics.heightPixels;
+		float deviceWidthInInches = displayMetrics.widthPixels
+				/ displayMetrics.xdpi;
+		boolean bothFit = landscapeMode
+				&& deviceWidthInInches >= PREFERRED_MAIN_WIDTH_INCHES
+						+ PREFERRED_OPTIONS_WIDTH_INCHES;
+
+		if (bothFit) {
+			mainWidth = displayMetrics.widthPixels
+					* (PREFERRED_MAIN_WIDTH_INCHES / TOTAL_WIDTH_INCHES);
+			optionsWidth = displayMetrics.widthPixels
+					* (PREFERRED_OPTIONS_WIDTH_INCHES / TOTAL_WIDTH_INCHES);
+		} else {
+			mainWidth = LayoutParams.MATCH_PARENT;
+			optionsWidth = LayoutParams.MATCH_PARENT;
+		}
+		SlidingPaneLayout.LayoutParams lp;
+		lp = new SlidingPaneLayout.LayoutParams((int) mainWidth,
+				LayoutParams.MATCH_PARENT);
+		lp.weight = 1;
+
+		container.addView(mainView, lp);
+
+		lp = new SlidingPaneLayout.LayoutParams((int) optionsWidth,
+				LayoutParams.MATCH_PARENT);
+		lp.weight = 0;
+
+		container.addView(auxilliaryView, lp);
+
+		if (!bothFit)
+			container.openPane();
+		return container;
+	}
+
+	private ViewGroup constructOptions() {
+		LinearLayout options = new LinearLayout(this);
+		options.setOrientation(LinearLayout.VERTICAL);
+
+		{
+			View.OnClickListener listener = new View.OnClickListener() {
+				public void onClick(View v) {
+					AlgorithmStepper.sharedInstance()
+							.processTestButtonPressed();
+				}
+			};
+
+			// Add some buttons
+			String labels[] = { "Alpha", "Bravo", "Charlie" };
+			for (int i = 0; i < labels.length; i++) {
+				Button b = new Button(this);
+				b.setText(labels[i]);
+				b.setOnClickListener(listener);
+				options.addView(b);
+			}
+		}
+		return options;
+	}
+
 	protected ViewGroup buildContentView() {
-		ViewGroup layout = super.buildContentView();
-		layout.addView(mAlgorithmStepper.controllerView(this));
-		return layout;
+		ViewGroup mainView = super.buildContentView();
+		mainView.addView(mAlgorithmStepper.controllerView(this));
+
+		if (false) {
+			return mainView;
+		} else {
+			ViewGroup options = constructOptions();
+			ViewGroup container;
+			if (false) {
+				container = constructDrawerLayout(mainView, options);
+			} else {
+				container = constructSlidingPaneLayout(mainView, options);
+			}
+			return container;
+		}
 	}
 
 	protected final GLSurfaceView buildOpenGLView() {
--- GeometryApp/src/com/js/samplealgorithm/SampleAlgorithm.java
+++ GeometryApp/src/com/js/samplealgorithm/SampleAlgorithm.java
@@ -8,8 +8,14 @@ import com.js.geometry.Polygon;
 import com.js.geometry.PolygonTriangulator;
 import com.js.geometryapp.AlgorithmRenderer;
 import com.js.geometryapp.AlgorithmStepper;
+import static com.js.basic.Tools.*;
 
 public class SampleAlgorithm implements AlgorithmStepper.Delegate {
+
+	public SampleAlgorithm() {
+		doNothing();
+		mStepper = AlgorithmStepper.sharedInstance();
+	}
 
 	public void setView(GLSurfaceView view, AlgorithmRenderer renderer) {
 		mView = view;
@@ -32,13 +38,30 @@ public class SampleAlgorithm implements AlgorithmStepper.Delegate {
 		mView.requestRender();
 	}
 
+	@Override
+	public void optionsChanged() {
+		prepareInput();
+		mStepper.resetStep();
+	}
+
 	public int getFrameNumber() {
 		return mAnimFrame;
 	}
 
 	private void prepareInput() {
 		mContext = GeometryContext.contextWithRandomSeed(1965);
-		mPolygon = Polygon.testPolygon(mContext, Polygon.TESTPOLY_DRAGON_X + 6);
+		int testPolys[] = { //
+		//
+				Polygon.TESTPOLY_DRAGON_X + 6,//
+				Polygon.TESTPOLY_CONCAVE_BLOB,//
+				Polygon.TESTPOLY_DRAGON_X, //
+				Polygon.TESTPOLY_Y_EQUALS_X_SQUARED,//
+				Polygon.TESTPOLY_LARGE_RECTANGLE,//
+				Polygon.TESTPOLY_DRAGON_X + 8,//
+		};
+		int choice = mStepper.getTestButtonPressCount() % testPolys.length;
+
+		mPolygon = Polygon.testPolygon(mContext, testPolys[choice]);
 		mPolygon.rotateBy(16 * MyMath.M_DEG);
 		mPolygon.transformToFitRect(mRenderer.algorithmRect(), false);
 	}
@@ -48,4 +71,5 @@ public class SampleAlgorithm implements AlgorithmStepper.Delegate {
 	private int mAnimFrame;
 	private GLSurfaceView mView;
 	private AlgorithmRenderer mRenderer;
+	private AlgorithmStepper mStepper;
 }

59e587b67b68416a8cf3b44e2829fbd5f0a130fe
Jeff Sember <jpsember@gmail.com>
2014-10-04 12:28:24 -0700

    Issue #92: added parseObject() utility method to JSONTools

--- GeometryApp/src/com/js/basic/JSONTools.java
+++ GeometryApp/src/com/js/basic/JSONTools.java
@@ -1,6 +1,21 @@
 package com.js.basic;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import static com.js.basic.Tools.*;
+
 public class JSONTools {
+
+	public static class JSONError extends RuntimeException {
+		public JSONError(JSONException e) {
+			super(e);
+		}
+	}
 
 	/**
 	 * Utility method that exchanges single and double quotes, to allow easier
@@ -23,4 +38,27 @@ public class JSONTools {
 		return sb.toString();
 	}
 
+	/**
+	 * Parse a JSONObject from a string, return as map
+	 * 
+	 * @param script
+	 * @return map of String -> Object
+	 * @throws JSONError
+	 */
+	public static Map<String, Object> parseObject(String script) {
+		try {
+			JSONObject object = (JSONObject) new JSONTokener(script)
+					.nextValue();
+			Map<String, Object> map = new HashMap();
+
+			Iterator<String> iter = object.keys();
+			while (iter.hasNext()) {
+				String key = iter.next();
+				map.put(key, object.get(key));
+			}
+			return map;
+		} catch (JSONException e) {
+			throw new JSONError(e);
+		}
+	}
 }
--- GeometryAppTest/src/com/js/jsontest/JSONTest.java
+++ GeometryAppTest/src/com/js/jsontest/JSONTest.java
@@ -1,5 +1,7 @@
 package com.js.jsontest;
 
+import java.util.Map;
+
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -17,6 +19,16 @@ public class JSONTest extends MyTestCase {
 		assertTrue(object.has("b"));
 		Object bValue = object.get("b");
 		assertTrue(bValue != null);
+		assertEquals(bValue, JSONObject.NULL);
+	}
+
+	public void testParseMap() {
+		String s = "{'a':5,'b':null,'c':17}";
+		Map<String, Object> map = parseObject(s);
+		assertTrue(map.containsKey("a"));
+		assertFalse(map.containsKey("d"));
+		assertTrue(map.containsKey("b"));
+		Object bValue = map.get("b");
 		assertEquals(bValue, JSONObject.NULL);
 	}
 
